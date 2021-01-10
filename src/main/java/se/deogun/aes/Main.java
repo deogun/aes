06f5ce0626acc6253762872f21be38f6c0dea27a
Daniel Deogun <daniel.deogun@omegapoint.se>
2021-01-10 17:24:01 +0100

    Complete redesign with unique nonce per encryption

--- src/main/java/se/deogun/aes/Main.java
+++ src/main/java/se/deogun/aes/Main.java
@@ -16,35 +16,30 @@ import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
 import static se.deogun.aes.AESFactory.aesGCM;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchAlgorithmException {
-        final var uuid1 = UUID.randomUUID();
-        final var transactionId1 = uuid1.toString();
-        final var uuid2 = UUID.randomUUID();
-        final var secret = new Secret(serialize(secretKey()));
-        final var aes1 = aesGCM(secret, new AAD(randomAlphanumeric(8192)));
-        final var aes2 = aesGCM(secret, new AAD(uuid2.toString()));
+    public static void main(String[] args) {
+        final var aes = aesGCM(new Secret(serialize(secretKey())), new AAD(randomAlphanumeric(8192)));
 
-        final Result<? extends Throwable, byte[], AESRejectReason> encryptResult1 = aes1.encrypt("some message 12345".getBytes());
+        final Result<? extends Throwable, byte[], AESRejectReason> encryptResult1 = aes.encrypt("some message 12345".getBytes());
         encryptResult1
                 .handle(success -> success
                         .accept(value -> System.out.println("successful encryption: " + Base64.getEncoder().encodeToString(value)))
                         .reject(reason -> System.out.println("rejected encryption: " + reason)))
                 .or(failure -> failure.printStackTrace());
 
-        final Result<? extends Throwable, byte[], AESRejectReason> encryptResult2 = aes2.encrypt("foo bar".getBytes());
+        final Result<? extends Throwable, byte[], AESRejectReason> encryptResult2 = aes.encrypt("foo bar".getBytes());
         encryptResult2
                 .handle(success -> success
                         .accept(value -> System.out.println("successful encryption: " + Base64.getEncoder().encodeToString(value)))
                         .reject(reason -> System.out.println("rejected encryption: " + reason)))
                 .or(failure -> failure.printStackTrace());
 
-        aes1.decrypt(encryptResult1.liftAccept())
+        aes.decrypt(encryptResult1.liftAccept())
                 .handle(success -> success
                         .accept(value -> System.out.println("successful decryption: " + new String(value)))
                         .reject(reason -> System.out.println("rejected decryption: " + reason)))
                 .or(failure -> failure.printStackTrace());
 
-        aes2.decrypt(encryptResult2.liftAccept())
+        aes.decrypt(encryptResult2.liftAccept())
                 .handle(success -> success
                         .accept(value -> System.out.println("successful decryption: " + new String(value)))
                         .reject(reason -> System.out.println("rejected decryption: " + reason)))
--- src/main/java/se/deogun/aes/modes/gcm/GCM.java
+++ src/main/java/se/deogun/aes/modes/gcm/GCM.java
@@ -4,47 +4,53 @@ import se.deogun.aes.modes.AESRejectReason;
 import se.deogun.aes.modes.Result;
 import se.deogun.aes.modes.Secret;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.*;
 import javax.crypto.spec.GCMParameterSpec;
 import java.nio.ByteBuffer;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.security.spec.AlgorithmParameterSpec;
 
 import static javax.crypto.Cipher.DECRYPT_MODE;
 import static javax.crypto.Cipher.ENCRYPT_MODE;
 import static org.apache.commons.lang3.Validate.notNull;
-import static se.deogun.aes.modes.AESRejectReason.UNABLE_TO_DECRYPT_DATA;
-import static se.deogun.aes.modes.AESRejectReason.UNABLE_TO_ENCRYPT_DATA;
+import static se.deogun.aes.modes.AESRejectReason.*;
 import static se.deogun.aes.modes.Result.*;
 
 public final class GCM {
+    private static final int START_INDEX_OF_ENCRYPTED_DATA = 12;
+    private static final int TAG_LENGTH_IN_BITS = 128;
+    private static final int START_INDEX_OF_IV = 0;
+    private static final int NUMBER_OF_IV_BYTES = 12;
+
+    private static final class UnableToCreateSecureRandom extends Exception {
+    }
+
     public final Result<Throwable, byte[], AESRejectReason> encrypt(final byte[] data, final Secret secret, final AAD aad) {
         notNull(data);
         notNull(secret);
         notNull(aad);
 
         try {
-            final var iv = new byte[12];
-            SecureRandom.getInstanceStrong().nextBytes(iv);
-
             final var cipher = Cipher.getInstance("AES/GCM/NoPadding");
-            final var parameters = new GCMParameterSpec(128, iv);
+            final byte[] initVector = initVector();
 
-            cipher.init(ENCRYPT_MODE, secret.keySpecification(), parameters);
+            cipher.init(ENCRYPT_MODE, secret.keySpecification(), new GCMParameterSpec(TAG_LENGTH_IN_BITS, initVector));
             cipher.updateAAD(aad.value());
-            final var cipherText = cipher.doFinal(data);
 
-            final var byteBuffer = ByteBuffer.allocate(iv.length + cipherText.length);
-            byteBuffer.put(iv);
-            byteBuffer.put(cipherText);
-            return accept(byteBuffer.array());
-            //return process(data, ENCRYPT_MODE);
-        } catch (BadPaddingException | IllegalBlockSizeException e) {
+            return accept(encryptedData(initVector, cipher.doFinal(data)));
+
+        } catch (UnableToCreateSecureRandom e) {
+            return reject(NO_SECURE_RANDOM_ALGORITHM_AVAILABLE_ON_THIS_SYSTEM);
+        } catch (IllegalStateException | BadPaddingException | IllegalBlockSizeException e) {
             return reject(UNABLE_TO_ENCRYPT_DATA);
-        } catch (Exception e) {
-            return failure(e);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            return reject(AES_GCM_NOT_AVAILABLE_ON_THIS_SYSTEM);
+        } catch (InvalidKeyException e) {
+            return reject(INVALID_GCM_KEY);
+        } catch (InvalidAlgorithmParameterException e) {
+            return reject(INVALID_GCM_PARAMETERS);
         }
     }
 
@@ -55,33 +61,37 @@ public final class GCM {
 
         try {
             final var cipher = Cipher.getInstance("AES/GCM/NoPadding");
-            AlgorithmParameterSpec spec = new GCMParameterSpec(128, data, 0, 12);
-            cipher.init(DECRYPT_MODE, secret.keySpecification(), spec);
-            cipher.updateAAD(aad.value());
-            return accept(cipher.doFinal(data, 12, data.length - 12));
 
-            //  return process(data, DECRYPT_MODE);
-        } catch (BadPaddingException | IllegalBlockSizeException e) {
+            cipher.init(DECRYPT_MODE, secret.keySpecification(), new GCMParameterSpec(TAG_LENGTH_IN_BITS, data, START_INDEX_OF_IV, NUMBER_OF_IV_BYTES));
+            cipher.updateAAD(aad.value());
+
+            return accept(cipher.doFinal(data, START_INDEX_OF_ENCRYPTED_DATA, data.length - 12));
+
+        } catch (IllegalStateException | BadPaddingException | IllegalBlockSizeException e) {
             return reject(UNABLE_TO_DECRYPT_DATA);
-        } catch (Exception e) {
-            return failure(e);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            return reject(AES_GCM_NOT_AVAILABLE_ON_THIS_SYSTEM);
+        } catch (InvalidKeyException e) {
+            return reject(INVALID_GCM_KEY);
+        } catch (InvalidAlgorithmParameterException e) {
+            return reject(INVALID_GCM_PARAMETERS);
         }
     }
 
-//    private Result<Throwable, byte[], AESRejectReason> process(final byte[] data, final int mode) throws BadPaddingException, IllegalBlockSizeException {
-//        try {
-//            final var cipher = Cipher.getInstance("AES/GCM/NoPadding");
-//            cipher.init(mode, context.secret(), context.parameters());
-//            cipher.updateAAD(context.aad());
-//
-//            return accept(cipher.doFinal(data));
-//
-//        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
-//            return reject(AES_GCM_NOT_AVAILABLE_ON_THIS_SYSTEM);
-//        } catch (InvalidKeyException e) {
-//            return reject(INVALID_GCM_KEY);
-//        } catch (InvalidAlgorithmParameterException e) {
-//            return reject(INVALID_GCM_PARAMETERS);
-//        }
-//    }
+    private byte[] initVector() throws UnableToCreateSecureRandom {
+        try {
+            final var nonce = new byte[12];
+            SecureRandom.getInstanceStrong().nextBytes(nonce);
+            return nonce;
+        } catch (NoSuchAlgorithmException e) {
+            throw new UnableToCreateSecureRandom();
+        }
+    }
+
+    private byte[] encryptedData(final byte[] iv, final byte[] encrypted) {
+        final var byteBuffer = ByteBuffer.allocate(iv.length + encrypted.length);
+        byteBuffer.put(iv);
+        byteBuffer.put(encrypted);
+        return byteBuffer.array();
+    }
 }
