22961b9cf79d3cf93abe6a2e65f58dcd2a9463bb
Daniel Deogun <daniel.deogun@omegapoint.se>
2021-12-05 16:02:01 +0100

    Added CBC integration test + refactoring

---
+++ src/test/java/se/deogun/aes/CBCIntegrationTest.java
@@ -0,0 +1,97 @@
+package se.deogun.aes;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import se.deogun.aes.api.AES;
+import se.deogun.aes.api.Secret;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphabetic;
+import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
+import static org.junit.jupiter.api.Assertions.*;
+import static se.deogun.aes.api.RejectReason.UNABLE_TO_DECRYPT_DATA;
+import static se.deogun.aes.api.Secret.secretFromBase64EncodedKey;
+import static se.deogun.aes.modes.common.SecretKeyFactory.base64EncodedKey;
+
+class CBCIntegrationTest {
+    @Test
+    void should_be_encrypted() {
+        final var data = randomAlphanumeric(100000).getBytes(UTF_8);
+
+        final var encrypted = cbc(secretFromBase64EncodedKey(base64EncodedKey())).encrypt(data).liftAccept();
+
+        assertFalse(Arrays.equals(data, encrypted));
+    }
+
+    @Test
+    void should_not_produce_same_encrypted_output_twice() {
+        final var data = randomAlphanumeric(100000).getBytes(UTF_8);
+
+        final var encryptedData1 = cbc(secretFromBase64EncodedKey(base64EncodedKey())).encrypt(data).liftAccept();
+        final var encryptedData2 = cbc(secretFromBase64EncodedKey(base64EncodedKey())).encrypt(data).liftAccept();
+
+        assertFalse(Arrays.equals(encryptedData1, encryptedData2));
+    }
+
+    @Test
+    void should_produce_the_same_output() {
+        final var data = randomAlphanumeric(100000).getBytes(UTF_8);
+        final var secret = secretFromBase64EncodedKey(base64EncodedKey());
+
+        final var decrypted1 = cbc(secret).decrypt(cbc(secret).encrypt(data).liftAccept()).liftAccept();
+        final var decrypted2 = cbc(secret).decrypt(cbc(secret).encrypt(data).liftAccept()).liftAccept();
+
+        assertArrayEquals(decrypted1, decrypted2);
+    }
+
+    @Disabled
+    @Test
+    void should_support_streams() {
+        final var data = randomAlphabetic(100000).getBytes(UTF_8);
+        final var outputStream = new ByteArrayOutputStream();
+        final var secret = secretFromBase64EncodedKey(base64EncodedKey());
+
+        cbc(secret).encrypt(data, outputStream).liftAccept();
+        final var result = cbc(secret).decrypt(new ByteArrayInputStream(outputStream.toByteArray())).liftAccept();
+
+        assertArrayEquals(data, result);
+    }
+
+    @Test
+    void should_fail_to_decrypt_because_of_wrong_secret() {
+        final var secret1 = secretFromBase64EncodedKey(base64EncodedKey());
+        final var secret2 = secretFromBase64EncodedKey(base64EncodedKey());
+        final var encrypted = cbc(secret1).encrypt(randomAlphanumeric(100000).getBytes(UTF_8)).liftAccept();
+
+        cbc(secret2)
+                .decrypt(encrypted)
+                .handle(success -> success
+                        .accept(value -> fail("unexpected successful decryption"))
+                        .reject(value -> assertEquals(UNABLE_TO_DECRYPT_DATA, value)))
+                .or(failure -> fail("unexpected exception"));
+    }
+
+    @Test
+    void should_use_stored_secret_to_encrypt_decrypt() throws IOException {
+        final var storedSecret = Files.readAllBytes(Paths.get("src", "test", "resources", "secret.b64enc"));
+        final var secret = secretFromBase64EncodedKey(storedSecret);
+        final var secretMessage = "This is my secret message";
+
+        final var encryptedMessage = cbc(secret).encrypt(secretMessage.getBytes(UTF_8)).liftAccept();
+        assertNotEquals(secretMessage, new String(encryptedMessage));
+
+        final var decryptedMessage = cbc(secret).decrypt(encryptedMessage).liftAccept();
+        assertEquals(secretMessage, new String(decryptedMessage));
+    }
+
+    private static AES cbc(final Secret secret) {
+        return AESFactory.aesCBC(secret);
+    }
+}
--- src/test/java/se/deogun/aes/GCMIntegrationTest.java
+++ src/test/java/se/deogun/aes/GCMIntegrationTest.java
@@ -2,6 +2,8 @@ package se.deogun.aes;
 
 import org.junit.jupiter.api.Test;
 import se.deogun.aes.api.AAD;
+import se.deogun.aes.api.AES;
+import se.deogun.aes.api.Secret;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -18,14 +20,14 @@ import static se.deogun.aes.api.RejectReason.UNABLE_TO_DECRYPT_DATA;
 import static se.deogun.aes.api.Secret.secretFromBase64EncodedKey;
 import static se.deogun.aes.modes.common.SecretKeyFactory.base64EncodedKey;
 
-class IntegrationTest {
+class GCMIntegrationTest {
     @Test
     void should_be_encrypted() {
         final var aad = new AAD(randomAlphanumeric(8192));
         final var data = randomAlphanumeric(100000).getBytes(UTF_8);
-        final var aes = AESFactory.aesGCM(secretFromBase64EncodedKey(base64EncodedKey()));
+        final var secret = secretFromBase64EncodedKey(base64EncodedKey());
 
-        final var encrypted = aes.encrypt(data, aad).liftAccept();
+        final var encrypted = gcm(secret).encrypt(data, aad).liftAccept();
 
         assertFalse(Arrays.equals(data, encrypted));
     }
@@ -34,10 +36,10 @@ class IntegrationTest {
     void should_not_produce_same_encrypted_output_twice() {
         final var aad = new AAD(randomAlphanumeric(8192));
         final var data = randomAlphanumeric(100000).getBytes(UTF_8);
-        final var aes = AESFactory.aesGCM(secretFromBase64EncodedKey(base64EncodedKey()));
+        final var secret = secretFromBase64EncodedKey(base64EncodedKey());
 
-        final var encryptedData1 = aes.encrypt(data, aad).liftAccept();
-        final var encryptedData2 = aes.encrypt(data, aad).liftAccept();
+        final var encryptedData1 = gcm(secret).encrypt(data, aad).liftAccept();
+        final var encryptedData2 = gcm(secret).encrypt(data, aad).liftAccept();
 
         assertFalse(Arrays.equals(encryptedData1, encryptedData2));
     }
@@ -46,10 +48,11 @@ class IntegrationTest {
     void should_produce_the_same_output() {
         final var aad = new AAD(randomAlphanumeric(8192));
         final var data = randomAlphanumeric(100000).getBytes(UTF_8);
-        final var aes = AESFactory.aesGCM(secretFromBase64EncodedKey(base64EncodedKey()));
+        final var secret = secretFromBase64EncodedKey(base64EncodedKey());
+        final var gcm = gcm(secret);
 
-        final var decrypted1 = aes.decrypt(aes.encrypt(data, aad).liftAccept(), aad).liftAccept();
-        final var decrypted2 = aes.decrypt(aes.encrypt(data, aad).liftAccept(), aad).liftAccept();
+        final var decrypted1 = gcm.decrypt(gcm.encrypt(data, aad).liftAccept(), aad).liftAccept();
+        final var decrypted2 = gcm.decrypt(gcm.encrypt(data, aad).liftAccept(), aad).liftAccept();
 
         assertArrayEquals(decrypted1, decrypted2);
     }
@@ -59,10 +62,10 @@ class IntegrationTest {
         final var aad = new AAD(randomAlphanumeric(8192));
         final var data = randomAlphabetic(100000).getBytes(UTF_8);
         final var outputStream = new ByteArrayOutputStream();
-        final var aes = AESFactory.aesGCM(secretFromBase64EncodedKey(base64EncodedKey()));
+        final var secret = secretFromBase64EncodedKey(base64EncodedKey());
 
-        aes.encrypt(data, outputStream, aad).liftAccept();
-        final var result = aes.decrypt(new ByteArrayInputStream(outputStream.toByteArray()), aad).liftAccept();
+        gcm(secret).encrypt(data, outputStream, aad).liftAccept();
+        final var result = gcm(secret).decrypt(new ByteArrayInputStream(outputStream.toByteArray()), aad).liftAccept();
 
         assertArrayEquals(data, result);
     }
@@ -70,11 +73,12 @@ class IntegrationTest {
     @Test
     void should_fail_to_decrypt_because_of_wrong_AAD() {
         final var aad = new AAD(randomAlphanumeric(8192));
-        final var aes = AESFactory.aesGCM(secretFromBase64EncodedKey(base64EncodedKey()));
+        final var secret = secretFromBase64EncodedKey(base64EncodedKey());
 
-        final var encrypted = aes.encrypt(randomAlphanumeric(100000).getBytes(UTF_8), aad).liftAccept();
+        final var encrypted = gcm(secret).encrypt(randomAlphanumeric(100000).getBytes(UTF_8), aad).liftAccept();
 
-        aes.decrypt(encrypted, new AAD(randomAlphabetic(10)))
+        gcm(secret)
+                .decrypt(encrypted, new AAD(randomAlphabetic(10)))
                 .handle(success -> success
                         .accept(value -> fail("unexpected successful decryption"))
                         .reject(value -> assertEquals(UNABLE_TO_DECRYPT_DATA, value)))
@@ -84,12 +88,12 @@ class IntegrationTest {
     @Test
     void should_fail_to_decrypt_because_of_wrong_secret() {
         final var aad = new AAD(randomAlphanumeric(8192));
-        final var aes1 = AESFactory.aesGCM(secretFromBase64EncodedKey(base64EncodedKey()));
-        final var aes2 = AESFactory.aesGCM(secretFromBase64EncodedKey(base64EncodedKey()));
+        final var gcm1 = gcm(secretFromBase64EncodedKey(base64EncodedKey()));
+        final var gcm2 = gcm(secretFromBase64EncodedKey(base64EncodedKey()));
 
-        final var encrypted = aes1.encrypt(randomAlphanumeric(100000).getBytes(UTF_8), aad).liftAccept();
+        final var encrypted = gcm1.encrypt(randomAlphanumeric(100000).getBytes(UTF_8), aad).liftAccept();
 
-        aes2.decrypt(encrypted, aad)
+        gcm2.decrypt(encrypted, aad)
                 .handle(success -> success
                         .accept(value -> fail("unexpected successful decryption"))
                         .reject(value -> assertEquals(UNABLE_TO_DECRYPT_DATA, value)))
@@ -98,16 +102,19 @@ class IntegrationTest {
 
     @Test
     void should_use_stored_secret_to_encrypt_decrypt() throws IOException {
-        final var storedSecret = Files.readAllBytes(Paths.get("src","test","resources", "secret.b64enc"));
+        final var storedSecret = Files.readAllBytes(Paths.get("src", "test", "resources", "secret.b64enc"));
         final var aad = new AAD(randomAlphanumeric(8192));
         final var secret = secretFromBase64EncodedKey(storedSecret);
-        final var aes = AESFactory.aesGCM(secret);
         final var secretMessage = "This is my secret message";
 
-        final var encryptedMessage = aes.encrypt(secretMessage.getBytes(UTF_8), aad).liftAccept();
+        final var encryptedMessage = gcm(secret).encrypt(secretMessage.getBytes(UTF_8), aad).liftAccept();
         assertNotEquals(secretMessage, new String(encryptedMessage));
 
-        final var decryptedMessage = aes.decrypt(encryptedMessage, aad).liftAccept();
+        final var decryptedMessage = gcm(secret).decrypt(encryptedMessage, aad).liftAccept();
         assertEquals(secretMessage, new String(decryptedMessage));
+    }
+
+    private static AES gcm(final Secret secret) {
+        return AESFactory.aesGCM(secret);
     }
 }
